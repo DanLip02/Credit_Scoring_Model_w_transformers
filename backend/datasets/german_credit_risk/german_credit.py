ebeff84f755e72ecc02b85d8c2fe745284c7547a
DanLip02 <danchik-lip@mail.ru>
2025-11-10 11:32:56 +0300

    upd fore german_credit_risk

--- backend/datasets/german_credit_risk/german_credit.py
+++ backend/datasets/german_credit_risk/german_credit.py
@@ -1,12 +1,15 @@
 from ucimlrepo import fetch_ucirepo
 import pandas as pd
+import ssl
+import certifi
+import io
+import urllib
 
-def load_german_credit_risk_UCI():
+def load_german_credit_risk_UCI_old():
 
     # fetch dataset
     statlog_german_credit_data = fetch_ucirepo(id=144)
 
-    # data (as pandas dataframes)
     X = statlog_german_credit_data.data.features
     y = statlog_german_credit_data.data.targets
 
@@ -39,15 +42,39 @@ def load_german_credit_risk_UCI():
         'Attribute20': 'foreign_worker'
     }
 
-    # Переименовываем колонки
     df = X.rename(columns=column_rename)
     #
-    # print("Переименованные колонки:")
     # print(df.columns.tolist())
     # print(f"\nРазмер данных: {df.shape}")
     finall = pd.concat([df, y], axis=1)
 
     return finall
 
+def load_german_credit_risk_UCI():
+    url = "https://archive.ics.uci.edu/ml/machine-learning-databases/statlog/german/german.data"
+
+    columns = [
+        "status_existing_checking", "duration_month", "credit_history", "purpose",
+        "credit_amount", "savings", "employment", "installment_rate",
+        "personal_status_sex", "other_debtors", "residence_since", "property",
+        "age", "other_installment_plans", "housing", "existing_credits",
+        "job", "people_liable", "telephone", "foreign_worker", "target"
+    ]
+
+    ctx = ssl.create_default_context(cafile=certifi.where())
+
+    raw = urllib.request.urlopen(url, context=ctx).read().decode("utf-8")
+
+    df = pd.read_csv(io.StringIO(raw), sep=" ", header=None, names=columns)
+
+    df.head()
+
+    # df.to_csv("german_credit_risk.csv")
+
+    # return df
+
 if __name__ == '__main__':
+
+    # load_german_credit_risk_UCI_old()
+
     load_german_credit_risk_UCI()
