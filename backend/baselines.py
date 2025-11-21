ec4852065d82b92853be9c5326a3a4d6cc4057e0
danlipatov <danila.lipatov@ratings.ru>
2025-11-21 13:08:06 +0300

    update german risk dataset + baselines

--- backend/baselines.py
+++ backend/baselines.py
@@ -1,4 +1,17 @@
 #### Module with basic models for credit risk modeling
+from sklearn.ensemble import (
+    VotingClassifier,
+    StackingClassifier,
+    BaggingClassifier,
+    RandomForestClassifier
+)
+from sklearn.linear_model import LogisticRegression, LinearRegression
+from xgboost import XGBClassifier
+from sklearn.metrics import f1_score, roc_auc_score, accuracy_score, classification_report
+# from sklearn.pipeline import Pipeline
+from imblearn.pipeline import Pipeline
+from catboost import CatBoostClassifier
+from imblearn.over_sampling import SMOTE
 
 """
 There will be some models that are really popular in credit risk modeling
@@ -16,3 +29,30 @@ Voting Classifier: Boosting + Bagging + Stacking + others
 Logisticregression / LinearRegression / RidgeRegression / LasssoRegression and others
 
 """
+
+
+def run_model(preprocessor, X_train, X_test, y_train, y_test, model, name="model"):
+    pipe = Pipeline([
+        ("preprocess", preprocessor),
+        ("smote",  SMOTE(random_state=42)),
+        ("clf", model)
+    ])
+    pipe.fit(X_train, y_train)
+
+    y_pred = pipe.predict(X_test)
+    y_proba = pipe.predict_proba(X_test)[:, 1]
+
+    auc = roc_auc_score(y_test, y_proba)
+    print(f"\n=== {name} ===")
+    print(f"AUC: {auc:.4f}")
+    print(classification_report(y_test, y_pred))
+    return {"AUC": auc, "Report": classification_report(y_test, y_pred, output_dict=True)}
+
+def get_baseline_models():
+    models = {
+        "Logistic Regression": LogisticRegression(max_iter=1000),
+        "Random Forest": RandomForestClassifier(n_estimators=300, class_weight="balanced", random_state=42),
+        "XGBoost": XGBClassifier(n_estimators=500, learning_rate=0.03, class_weight="balanced", random_state=42)
+    }
+    return models
+
---
+++ backend/main.py
@@ -0,0 +1,17 @@
+from data_explorer import load_data, split_data, get_preprocessor
+from baselines import run_model, get_baseline_models
+
+
+
+
+
+if __name__ == '__main__':
+    df = load_data("/Users/danilalipatov/Credit_Scoring_Model_w_transformers/backend/datasets/german_credit_risk/german_credit_risk.csv")
+    X_train, X_test, y_train, y_test = split_data(df)
+    preprocessor = get_preprocessor(X_train)
+
+    models = get_baseline_models()
+    results = {}
+
+    for name, model in models.items():
+        results[name] = run_model(preprocessor, X_train, X_test, y_train, y_test, model, name)
